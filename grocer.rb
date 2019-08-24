aec68b7538fd854e31bcc10a816e0e0dd031198a
Sergio Perez <sergio13prez@gmail.com>
2019-08-24 20:17:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,7 +24,7 @@ def apply_coupons(cart, coupons)
     if cart[item]
       if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
         cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance] , count: coupon[:num]}
-      elsif
+      elsif cart[item][:count]
       
 end
 
