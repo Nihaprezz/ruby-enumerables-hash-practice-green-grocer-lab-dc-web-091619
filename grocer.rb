4ce4cf6d8acecbd256e1e4ef6270fb32f9127e3c
Sergio Perez <sergio13prez@gmail.com>
2019-08-24 20:14:53 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,7 +23,7 @@ def apply_coupons(cart, coupons)
     item = coupon[:item]
     if cart[item]
       if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
-        cart["#{item} W/COUPON"] = {price: , clearance: , 
+        cart["#{item} W/COUPON"] = {price: , clearance: , count: }
 end
 
 def apply_clearance(cart)
